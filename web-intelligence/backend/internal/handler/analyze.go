63015d95fe465492ff3ee51871f9ae535758cefa
Maverick7t <moinaktarshaikh651@gmail.com>
2026-08-24 19:52:05 +0530

    struct for metadat holding

--- web-intelligence/backend/internal/handler/analyze.go
+++ web-intelligence/backend/internal/handler/analyze.go
@@ -21,3 +21,10 @@ type successResponse struct {
 	} `json:"result"`
 	Meta meta `json:"meta"`
 }
+
+type meta struct {
+	RequestID       string `json:"request_id"`
+	TotalDurationMs int64  `json:"total_duration_ms"`
+	FetchDurationMs int64  `json:"fetch_duration_ms"`
+	NimDurationMs   int64  `json:"nim_duration_ms"`
+}
