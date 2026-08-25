de3571368b09c3704855cb8fd38d48023b670efa
Maverick7t <moinaktarshaikh651@gmail.com>
2026-08-25 14:34:40 +0530

    application response and handling writeHeader gracefully

--- web-intelligence/backend/internal/handler/analyze.go
+++ web-intelligence/backend/internal/handler/analyze.go
@@ -105,4 +105,7 @@ func (a *analyzer) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 		NimDurationMs:   nimDuration.Milliseconds(),
 	}
 
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	_ = json.NewEncoder(w).Encode(resp)
 }
