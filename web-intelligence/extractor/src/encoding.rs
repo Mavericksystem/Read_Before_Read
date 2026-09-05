9506203c44319e27fb92fa6e62e5729f09ab74eb
Maverick7t <moinaktarshaikh651@gmail.com>
2026-09-05 10:14:19 +0530

    decode bytes with utf8

--- web-intelligence/extractor/src/encoding.rs
+++ web-intelligence/extractor/src/encoding.rs
@@ -4,4 +4,10 @@ pub fn decode(bytes: &[u8], content_type_header: &str) -> String {
     if let Some(enc) = encoding_from_content_type(content_type_header) {
         return decode_with(bytes, enc);
     }
+
+    if let Some(enc) = encoding_from_meta_sniff(bytes) {
+        return decode_with(bytes, enc);
+    }
+
+    decode_with(bytes, encoding_rs::UTF_8)
 }
