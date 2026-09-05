35218537aaa722504adc09c08e525e8b7a303c13
Maverick7t <moinaktarshaikh651@gmail.com>
2026-09-05 11:56:51 +0530

    performence efficency of html extraction using criterion framework

--- web-intelligence/extractor/benches/extraction_bench.rs
+++ web-intelligence/extractor/benches/extraction_bench.rs
@@ -9,7 +9,19 @@ fn small_page_html() -> String {
         <article><p>{}</p></article>
         <footer>Copyright 2026</footer>
         </body></html>"#,
-        "Lore ipsum dolor sit amet, ".repeat(20)
+        "This is the best low level programming language, ".repeat(20)
     )
 }
 
+fn larger_page_html() -> String {
+    format!(
+        r#"<html><head><Larger Page</title></head><body>
+        <nav>{}</nav>
+        <article>{}</article>
+        <footer>{}</footer>
+        </body></html>"#,
+        "<a href=\"/x\">Link</a>".repeat(3000),
+        "<p>This is rust language designed for safety and performance. </p>".repeat(3000),
+        "<p>Copyright 2026</p>".repeat(30)
+    )
+}
