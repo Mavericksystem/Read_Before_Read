38abc06811017555e31d152ad38af1ff9fa29eb1
Maverick7t <moinaktarshaikh651@gmail.com>
2026-09-08 20:09:56 +0530

    create and start pool of size worker

--- web-intelligence/backend/internal/pool/pool.go
+++ web-intelligence/backend/internal/pool/pool.go
@@ -1,9 +1,7 @@
 package pool
 
 import (
-	"context"
 	"fmt"
-	"log"
 	"sync"
 )
 
@@ -26,3 +24,19 @@ func New(binaryPath string, size int) (*Pool, error) {
 		size:       size,
 		free:       make(chan *worker, size),
 	}
+
+	started := make([]*worker, 0, size)
+	for i := 0; i < size; i++ {
+		w, err := newWorker(binaryPath)
+		if err != nil {
+			for _, sw := range started {
+				sw.kill()
+			}
+			return nil, fmt.Errorf("pool: starting worker %d/%d: %w", i+1, size, err)
+		}
+		started = append(started, w)
+		p.free <- w
+	}
+
+	return p, nil
+}
