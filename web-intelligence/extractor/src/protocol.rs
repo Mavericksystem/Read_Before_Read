c187a1e105145f7dfa521908b8d2f6771fc31cb4
Maverick7t <moinaktarshaikh651@gmail.com>
2026-09-08 16:29:51 +0530

    desearilizarion of json

--- web-intelligence/extractor/src/protocol.rs
+++ web-intelligence/extractor/src/protocol.rs
@@ -7,4 +7,16 @@ pub fn read_job<T: DeserializeOwned>(
 ) -> io::Result<Option<T>> {
     let mut line = String::new();
     let bytes_read = reader.read_line(&mut line)?;
+
+    if bytes_read == 0 {
+        retrun Ok(None);
+    }
+
+    let trimmed = line.trim();
+    if trimmed.is_empty() {
+        return read_job(reader);
+    }
+
+    
+
 }
