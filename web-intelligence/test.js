2d01c0fb739b0a355c64a7ade89060617735638d
Maverick7t <moinaktarshaikh651@gmail.com>
2026-09-11 20:00:04 +0530

    changeing test

--- web-intelligence/test.js
+++ web-intelligence/test.js
@@ -1,7 +1,21 @@
 import http from 'k6/http';
-export const options = { vus: 5, duration: '90s' };
+
+export const options = {
+    vus: 5,
+    duration: '90s',
+};
+
 export default function () {
-    http.post('http://localhost:8080/api/v1/analyze',
-        JSON.stringify({ url: "https://example.com" }),
-        { headers: { 'Content-Type': 'application/json' } });
+    http.post(
+        'http://localhost:8080/api/v1/analyze',
+        JSON.stringify({
+            url: 'https://example.com',
+        }),
+        {
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            timeout: '90s',
+        },
+    );
 }
